ed22b7f94678ecb9ab093acab685d58779c44c56
feline123gibhubs <134892253+feline123gibhubs@users.noreply.github.com>
2025-03-15 01:40:34 +0700

    CRUD Notification

---
+++ ChildVaccineScheduleTrackingSystem/RazorPage/Pages/Notification/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage.Pages.Notification
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
